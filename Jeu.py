8d8ebf8b42bdff245d4825de9f863b160e5d79cd
Lienep0 <119780411+Lienep0@users.noreply.github.com>
2023-03-31 09:55:35 +0200

    Add files via upload
    Clean up le code, séparé les projectiles, ajouté les astéroides

--- Jeu.py
+++ Jeu.py
@@ -1,63 +1,88 @@
 import pyxel
 
-tirs_liste = []
-
 class Player:
     def __init__(self):
-        self.ship_x = 60
-        self.ship_y = 60
+        self.x = 60
+        self.y = 60
+        self.fireRateTimer = 5
+        self.fireRateCooldown = 0
     
     def player_controls(self):
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.ship_x<120:
-            self.ship_x += 2
-        if pyxel.btn(pyxel.KEY_LEFT) and self.ship_x>0:
-            self.ship_x += -2
-        if pyxel.btn(pyxel.KEY_DOWN) and self.ship_y<120:
-            self.ship_y += 2
-        if pyxel.btn(pyxel.KEY_UP) and self.ship_y>0:
-            self.ship_y += -2
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.x < 96:
+            self.x += 2
+        if pyxel.btn(pyxel.KEY_LEFT) and self.x > 0:
+            self.x -= 2
+        if pyxel.btn(pyxel.KEY_DOWN) and self.y < 132:
+            self.y += 2
+        if pyxel.btn(pyxel.KEY_UP) and self.y > 0:
+            self.y -= 2
+        if pyxel.btn(pyxel.KEY_SPACE) and self.fireRateCooldown <= 0:
+            self.fireRateCooldown = self.fireRateTimer
+            bullet = Bullet(player.x + 1, player.y)
+            bullet2 = Bullet(player.x + 6, player.y)
+            bullet_list.extend([bullet,bullet2])
 
     def update(self):
+        self.fireRateCooldown -= 1
         self.player_controls()
-        if pyxel.btn(pyxel.KEY_SPACE):
-            tir = Bullet(player.ship_x, player.ship_y-8)
-            tirs_liste.append(tir)
 
     def draw(self):
-        pyxel.blt(self.ship_x, self.ship_y, 0, 0, 8, 8, 8)
+        pyxel.blt(self.x, self.y, 0, 0, 8, 8, 8)
 
 class Bullet:
     def __init__(self,x,y):
-        self.x=x
-        self.y=y
+        self.x = x
+        self.y = y
 
     def update(self):
         self.y -= 3
-        if self.y <-8:
-            tirs_liste.remove(self)
+        if self.y < -8:
+            bullet_list.remove(self)
 
     def draw(self):
-        for tir in tirs_liste:
-                pyxel.blt(tir.x, tir.y, 0, 8, 0, 8, 8)
+        for bullet in bullet_list:
+                pyxel.blt(bullet.x, bullet.y, 0, 0, 0, 1, 8)
+
+class Asteroid():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        for asteroid in asteroid_list:
+                pyxel.blt(asteroid.x, asteroid.y, 0, 32, 16, 8, 8)
 
 class App:
     def __init__(self):
         global player
+        global bullet_list
+        global asteroid_list
+
+        bullet_list = []
+        asteroid = Asteroid(0,0)
+        asteroid_list = [asteroid]
         player = Player()
-        pyxel.init(128, 128, title="SpaceFucker")
+
+        pyxel.init(104, 140, title="SpaceFucker")
         pyxel.load("Jeu.pyxres")
         pyxel.run(self.update, self.draw)
     
     def update(self):
         player.update()
-        for tir in tirs_liste:
-            tir.update()
+        for bullet in bullet_list:
+            bullet.update()
 
     def draw(self):
         pyxel.cls(0)
+        for asteroid in asteroid_list:
+            asteroid.draw()
+        for bullet in bullet_list:
+            bullet.draw()
         player.draw()
-        for tir in tirs_liste:
-            tir.draw()
+        
         
 if __name__ == "__main__":
     App()
