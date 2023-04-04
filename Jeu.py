ad73c257fe066e7953cfe9e1f3ea577067edbb4e
Lienep0 <119780411+Lienep0@users.noreply.github.com>
2023-04-04 11:49:45 +0200

    Add files via upload
    Added stars, asteroids, collisions

--- Jeu.py
+++ Jeu.py
@@ -1,4 +1,5 @@
 import pyxel
+from random import *
 
 class Player:
     def __init__(self):
@@ -6,7 +7,7 @@ class Player:
         self.y = 60
         self.fireRateTimer = 5
         self.fireRateCooldown = 0
-    
+
     def player_controls(self):
         if pyxel.btn(pyxel.KEY_RIGHT) and self.x < 96:
             self.x += 2
@@ -17,9 +18,10 @@ class Player:
         if pyxel.btn(pyxel.KEY_UP) and self.y > 0:
             self.y -= 2
         if pyxel.btn(pyxel.KEY_SPACE) and self.fireRateCooldown <= 0:
+            generate()
             self.fireRateCooldown = self.fireRateTimer
-            bullet = Bullet(player.x + 1, player.y)
-            bullet2 = Bullet(player.x + 6, player.y)
+            bullet = Bullet(self.x + 1, self.y)
+            bullet2 = Bullet(self.x + 6, self.y)
             bullet_list.extend([bullet,bullet2])
 
     def update(self):
@@ -28,6 +30,22 @@ class Player:
 
     def draw(self):
         pyxel.blt(self.x, self.y, 0, 0, 8, 8, 8)
+
+class Star:
+    def __init__(self,x,y,speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def update(self):
+        self.y += self.speed
+        if self.y > 138:
+            self.y = 0
+            self.x = randint(2,94)
+            self.speed = uniform(1,3)
+
+    def draw(self):
+        pyxel.pset(self.x, self.y, 1)
 
 class Bullet:
     def __init__(self,x,y):
@@ -40,49 +58,68 @@ class Bullet:
             bullet_list.remove(self)
 
     def draw(self):
-        for bullet in bullet_list:
-                pyxel.blt(bullet.x, bullet.y, 0, 0, 0, 1, 8)
+        pyxel.blt(self.x, self.y, 0, 0, 0, 1, 8)
 
-class Asteroid():
-    def __init__(self,x,y):
+class Asteroid:
+    def __init__(self,x):
         self.x = x
-        self.y = y
+        self.y = 0
 
     def update(self):
-        pass
+        self.y += 1
+        for bullet in bullet_list:
+            if abs(self.x - bullet.x) < 2 and abs(self.y - bullet.y) < 8:
+                asteroid_list.remove(self)
+
 
     def draw(self):
-        for asteroid in asteroid_list:
-                pyxel.blt(asteroid.x, asteroid.y, 0, 32, 16, 8, 8)
+        pyxel.blt(self.x, self.y, 0, 32, 16, 8, 8)
+
+def generate():
+    global asteroid_list
+    asteroid_list += [Asteroid(randint(0,96))]
 
 class App:
     def __init__(self):
         global player
         global bullet_list
         global asteroid_list
+        global star_list
 
         bullet_list = []
-        asteroid = Asteroid(0,0)
-        asteroid_list = [asteroid]
+        asteroid_list = []
+        star_list = []
         player = Player()
+        self.framecount = 0
+        self.asteroid_cooldown = 5
 
-        pyxel.init(104, 140, title="SpaceFucker")
+        for _ in range(30):
+            star_list+=[Star(randint(2,102),randint(2,138),uniform(1,3))]
+
+        pyxel.init(104, 140, title="Space Survivor")
         pyxel.load("Jeu.pyxres")
         pyxel.run(self.update, self.draw)
-    
+
     def update(self):
+        self.framecount += 1
+        if self.framecount % self.asteroid_cooldown == 0: generate()
         player.update()
         for bullet in bullet_list:
             bullet.update()
+        for star in star_list:
+            star.update()
+        for asteroid in asteroid_list:
+            asteroid.update()
 
     def draw(self):
         pyxel.cls(0)
+        for star in star_list:
+            star.draw()
         for asteroid in asteroid_list:
             asteroid.draw()
         for bullet in bullet_list:
             bullet.draw()
         player.draw()
-        
-        
+
 if __name__ == "__main__":
     App()
