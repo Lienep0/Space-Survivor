44779b8f4844e1151dc0d2255789682f2e12bfcc
Lienep <neilp@live.fr>
2023-04-10 22:15:53 +0200

    added TODOs

---
+++ todos.py
@@ -0,0 +1,16 @@
+# Done
+# TODO : A lot of stuff
+
+# Imperative
+# TODO : Add level up sfx
+# TODO : Add more upgrades (Explosion, Piercing, Triple shot, Fullscreen clear bomb)
+# TODO : Add attacks to miniboss (spread shot pattern ?)
+# TODO : Wave Counter UI/Behavior
+# TODO : Score System
+# TODO : High Score Tracker
+# TODO : High Score name Inputter
+# TODO : GAMEMANAGER
+
+# Optionnal
+# TODO : Add music
+# TODO : Controls Tutorial Menu ? (otherwise just put in readme)
