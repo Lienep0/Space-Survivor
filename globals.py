19adcd5f96c912cf80a6b4876e4b8dae26b376f8
Lienep <neilp@live.fr>
2023-04-21 13:31:31 +0200

    added score global

--- globals.py
+++ globals.py
@@ -1,11 +1,15 @@
 framecount = 0 # The number of frames since game start
 state = "MENU" # The current state of the game, written in all caps
+score = 0 
 
 def get_state():
     return state
 
 def get_framecount():
     return framecount
+
+def get_score():
+    return score
 
 def set_state(new_state):
     global state
@@ -18,3 +22,7 @@ def update_framecount():
 def reset_framecount():
     global framecount
     framecount = 0
+
+def add_score(points):
+    global score
+    score += points
