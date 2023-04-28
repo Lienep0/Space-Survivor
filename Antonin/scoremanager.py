311e4ad03bfea098a8dd01e3a28cf91529d687d2
PoserCanary <nino.uwuwu@gmail.com>
2023-04-28 18:16:26 +0200

    j'ai encore oublier de save

--- Antonin/scoremanager.py
+++ Antonin/scoremanager.py
@@ -5,17 +5,17 @@ class scoreManager:
     def __init__(self):
         self.data = json.load(open("scores.json", "r"))
         self.dic_data = {}
-        for x in self.data.values():
-            self.dic_data[str(x["name"])] = (int(x["score"]))
+        for value in self.data.values():
+            self.dic_data[str(value["name"])] = (int(value["score"]))
 
 
     def update(self):
         i = 1
         self.data = {}
-        for x,y in self.dic_data.items() :
+        for key,value in self.dic_data.items() :
             z = "player"
             z += " " + str(i)
-            self.data[z]={"name" : x, "score" : str(y)}
+            self.data[z]={"name" : key, "score" : str(value)}
             i += 1
 
         with open("scores.json", "w") as write_file:
@@ -23,16 +23,19 @@ class scoreManager:
     
     def draw(self):
         i = 0
-        for x in self.dic_data.keys():
-            pyxel.text(30, 65 + i, x , 7)
-            pyxel.text(50, 65 + i, str(self.dic_data[x]) , 7)
+        for key in self.dic_data.keys():
+            pyxel.text(30, 65 + i, key , 7)
+            pyxel.text(50, 65 + i, str(self.dic_data[key]) , 7)
             i += 10
 
     def reset(self):
-        self.data = json.load(open("scores.json", "r"))
+        self.data = {"player 1": {"name": "XXX", "score": "0000000"},
+                "player 2": {"name": "XXX", "score": "0000000"},
+                "player 3": {"name": "XXX", "score": "0000000"}}
         self.dic_data = {}
-        for x in self.data.values():
-            self.dic_data[str(x["name"])] = (int(x["score"]))
+        for value in self.data.values():
+            self.dic_data[str(value["name"])] = (int(value["score"]))
+        self.update()
 
 
 Scores = scoreManager()
